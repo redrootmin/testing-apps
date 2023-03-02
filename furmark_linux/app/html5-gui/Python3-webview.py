6a85c58e7e0d21511a86bd8fb1692f56a6d472b2
redrootmin <redroot@gmail.com>
2023-03-02 20:12:57 +0300

    update test app

--- furmark_linux/app/html5-gui/Python3-webview.py
+++ furmark_linux/app/html5-gui/Python3-webview.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 import webview
-webview.create_window('Furmark Linux gui', 'Minimum window size', 'html5-gui.html', fullscreen=True, min_size=(1280, 720))
+webview.create_window('Furmark Linux gui', 'html5-gui.html', min_size=(1280, 720))
 webview.start()
