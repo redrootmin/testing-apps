315e81d549aab4ca5cda0b593c857823cfd34fbd
redrootmin <redroot@gmail.com>
2023-08-18 08:05:22 +0300

    add new testing app

---
+++ mesa-switcher2/scripts/menu-backup-game.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#creator by RedRoot(Yaciyna Mikhail) for GAMER STATION [on linux] and Gaming Community OS Linux
+# GPL-3.0 License 
+
+HERE="$(dirname "$(readlink -f "$0")")"
+echo "Скрипт запущен в папке: $HERE"
+name_cut="/scripts"
+app_path=`echo ${HERE} | sed "s|${name_cut}||g"`
+echo "Папка программы: $app_path"
+image1="$app_path/images/logo-image512.png"
+#
+yad --center --window-icon=$HERE/DATA/Icons/SSD.png --title="Архивация игры Minetest" --form --width=400 \
+--image="$image1" --image-on-top \
+--field="ОБЫЧНЫЙ ЗАПУСК ИГРЫ":fbtn "$HERE/fstrim.sh" \
+--field="СДЕЛАТЬ АРХИВАЦИЮ СОХРАНЕНИЙ В ОБЛАКО YANDEX":fbtn "$HERE/fstrim.sh" \
+--field="ЗАГРУЗИТЬ СОХРАНЕНИЯ ИЗ ОБЛОКА YANDEX":fbtn "$HERE/fstrim.sh" \
+--field="ВОССТАНОВИТЬ ДАННЫЕ ИЗ БОЛЕЕ СТАРОГО ЛОКАЛЬНОГО АРХИВА":fbtn "$HERE/fstrim.sh" \
+--field="ВОССТАНОВИТЬ ДАННЫЕ ИЗ БОЛЕЕ СТАРОГО АРХИВА В YANDEX ОБЛАКЕ":fbtn "$HERE/fstrim.sh" \
+--button="О СКРИПТЕ":$HERE/about.sh --button="ВЫХОД":1 --buttons-layout=right
