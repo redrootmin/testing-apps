bb142e5ada509cce87cf56222bd0525cf990a4ce
redrootmin <redroot@gmail.com>
2024-07-19 16:54:41 +0300

    add new samples

---
+++ mks/sample/notebook-gtk.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#creator by RedRoot(Yaciyna Mikhail) for GAMER STATION [on linux] and Gaming Community OS Linux
+# GPL-3.0 License 
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Simple Notebook Example")
+        self.set_border_width(3)
+
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
+
+        self.page1 = Gtk.Box()
+        self.page1.set_border_width(10)
+        self.page1.add(Gtk.Label(label="Default Page!"))
+        self.notebook.append_page(self.page1, Gtk.Label(label="Plain Title"))
+
+        self.page2 = Gtk.Box()
+        self.page2.set_border_width(10)
+        self.page2.add(Gtk.Label(label="A page with an image for a Title."))
+        self.notebook.append_page(
+            self.page2, Gtk.Image.new_from_icon_name("help-about", Gtk.IconSize.MENU)
+        )
+
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
