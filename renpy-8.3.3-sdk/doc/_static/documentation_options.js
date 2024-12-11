a7ed6ffbeef6f9d1dc7204da847905f7f68c1173
redrootmin <redroot@gmail.com>
2024-12-11 16:23:03 +0300

    update-renpy

--- renpy-8.3.3-sdk/doc/_static/documentation_options.js
+++ renpy-8.3.3-sdk/doc/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '8.3.3',
+    VERSION: '8.3.4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- renpy-8.3.3-sdk/renpy/vc_version.py
+++ renpy-8.3.3-sdk/renpy/vc_version.py
@@ -1,5 +1,5 @@
 branch = 'fix'
 nightly = False
 official = True
-version = '8.3.3.24111502'
+version = '8.3.4.24120703'
 version_name = 'Second Star to the Right'
